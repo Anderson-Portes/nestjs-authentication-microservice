d2e225760b07f37a4723fcee678081ea77f90511
Anderson-Portes <portsanderson@gmail.com>
2024-02-25 15:49:28 -0300

    refactor: remove AppController and AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './shared/prisma/prisma.module';
 import { RolesModule } from './api/admin/roles/roles.module';
 import { UsersModule } from './api/admin/users/users.module';
@@ -8,7 +6,7 @@ import { AuthModule } from './api/auth/auth.module';
 
 @Module({
   imports: [PrismaModule, RolesModule, UsersModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
