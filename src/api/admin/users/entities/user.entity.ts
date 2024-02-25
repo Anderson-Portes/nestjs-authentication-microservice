f518bf37445ab67ea5906a45f8aa651e078dce33
Anderson-Portes <portsanderson@gmail.com>
2024-02-25 12:35:31 -0300

    feat: created crud of users and modify return of roles.controller create function

--- src/api/admin/users/entities/user.entity.ts
+++ src/api/admin/users/entities/user.entity.ts
@@ -1,1 +1,11 @@
-export class User {}
+import { Prisma } from '@prisma/client';
+
+export class User implements Prisma.UserUncheckedCreateInput {
+  id?: string;
+  name: string;
+  email: string;
+  password: string;
+  roleId: string;
+  createdAt?: string | Date;
+  updatedAt?: string | Date;
+}
