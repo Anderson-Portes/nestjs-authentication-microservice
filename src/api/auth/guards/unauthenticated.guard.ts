ce11b322f025f5195bdcaaa63e677e5e713aa194
Anderson-Portes <portsanderson@gmail.com>
2024-02-25 13:18:41 -0300

    feat: create guards and decorators to verify if user are authenticated or not

---
+++ src/api/auth/guards/unauthenticated.guard.ts
@@ -0,0 +1,32 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+
+@Injectable()
+export class UnauthenticatedGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      return true;
+    }
+    try {
+      await this.jwtService.verifyAsync(token);
+    } catch {
+      return true;
+    }
+    throw new UnauthorizedException();
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
