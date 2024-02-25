cd7fb2e402121ea5f999f95b95e57e0a0e0b5a34
Anderson-Portes <portsanderson@gmail.com>
2024-02-25 12:00:31 -0300

    feat: addes scaffold of users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './shared/prisma/prisma.module';
 import { RolesModule } from './api/admin/roles/roles.module';
+import { UsersModule } from './api/admin/users/users.module';
 
 @Module({
-  imports: [PrismaModule, RolesModule],
+  imports: [PrismaModule, RolesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
